f320e554b9d55293b4376607069dc28b140ccfd0
Claudio Carigé <ccarige@gmail.com>
2022-08-17 12:22:18 -0300

    Commit inicial 2

--- primeiroprojetoJavascript/assets/js/scripts.js
+++ primeiroprojetoJavascript/assets/js/scripts.js
@@ -2,15 +2,21 @@ var current_number_w = document.getElementById('currentNumber');
 var current_number = 0;
 
 function increment(){
-    if ((0 <= current_number) && (current_number< 10)){
+    if (current_number< 11){
         current_number = current_number + 1;
         current_number_w.innerHTML = current_number;
+    }
+    if (current_number === 11){
+        alert("Só pode incrementa até o número 11.")
     }
 }
 
 function decrement(){
-    if ((0 < current_number) && (current_number<= 10)){
+    if (0 < current_number){
         current_number = current_number - 1;
         current_number_w.innerHTML = current_number;
+    }
+    if (current_number <= 0){
+        alert("Não decrementa para número negativo.")
     }
 }
